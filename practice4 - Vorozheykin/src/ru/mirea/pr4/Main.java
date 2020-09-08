d019535e644980b84c6171855b51df815a5c27ad
Danil Vorozheykin <danilvda@gmail.com>
2020-09-08 09:19:37 +0300

    Add Practice 4

---
+++ practice4 - Vorozheykin/src/ru/mirea/pr4/Main.java
@@ -0,0 +1,12 @@
+package ru.mirea.pr4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Team milan = new Team("AC Milan");;
+		Team madrid = new Team("Real Madrid");
+
+		new MatchResults(milan, madrid)
+				.setVisible(true);
+	}
+}
